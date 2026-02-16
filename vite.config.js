b5f918985f3244e76c7625335c0c716bfdf11e1e
surajahirzde <sy241527@gmail.com>
2026-02-16 13:31:41 +0530

    sqwd

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
 export default defineConfig({
-  base: './',  
- 
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-  }
+  plugins: [
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler']],
+      },
+    }),
+  ],
 })
